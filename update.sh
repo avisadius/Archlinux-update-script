8b9eefbd6936e7ad15c24f9baca8671b53d0c51d
avisadius <44080415+avisadius@users.noreply.github.com>
2026-03-29 12:42:00 +0700

    Add update script for package management and cleanup
    This script manages package updates and cleanup for Arch-based systems. It ensures no conflicting processes are running before performing system maintenance tasks.

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo rm -f /var/lib/pacman/db.lck
+
+if ps aux | grep -q '[p]acman'; then
+    echo "Masih ada proses pacman yang berjalan. Menghentikan..."
+    sudo pkill -9 pacman
+    sleep 2
+fi
+
+if ps aux | grep -q '[y]ay'; then
+    echo "Masih ada proses yay yang berjalan. Menghentikan..."
+    sudo pkill -9 yay
+    sleep 2
+fi
+
+sudo shiny-mirrors refresh && \
+yay -Syyu --noconfirm && \
+
+if [ -n "$(pacman -Qtdq)" ]; then sudo pacman -Rns $(pacman -Qtdq) --noconfirm; fi && \
+
+sudo pacman -Sc --noconfirm && \
+
+yay -Sc --noconfirm && \
+
+flatpak uninstall --unused -y && \
+flatpak repair && \
+
+sudo grub-mkconfig -o /boot/grub/grub.cfg
